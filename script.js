b45ddbbcfd3324528813eef5f225f409d0d6ae2d
stoic-paki <taha.s200526@gmail.com>
2025-04-12 14:03:53 +1000

    adding the etch e sketch functionality left reset button and user input for grids.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const root_chl = document.querySelector("#root-chl")
 root_chl.style.cssText="background:red;"
+let box_col;
 
 for(let i=1; i<=16; i++){
     // this creates the rows of the grid
@@ -10,9 +11,17 @@ for(let i=1; i<=16; i++){
 
     // this creates the columns of each row which are 16
     for(let o=1; o<=16; o++){
-        let box_col = document.createElement("div")
+        box_col = document.createElement("div")
         box_col.classList.add("box_col")
         // box_col.textContent=o
         box_row.appendChild(box_col)
     }
 }
+
+root_chl.addEventListener("mouseover", (e)=>{
+    if(e.target.classList.contains("box_col")) {
+        console.log("hello world")
+        console.log(e)
+        e.target.style.cssText="background:blue;"
+    }
+})
